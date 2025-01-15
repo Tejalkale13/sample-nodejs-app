a56acb384e7e56932c17fc674f06c51bb1489dc0
Tejalkale13 <166007676+Tejalkale13@users.noreply.github.com>
2025-01-15 18:23:33 +0530

    Add files via upload

---
+++ tests/app.test.js
@@ -0,0 +1,10 @@
+const request = require('supertest');
+const app = require('../app'); // Import the Express app for testing
+
+describe('GET /', () => {
+    it('should return "Hello, Jenkins Pipeline!"', async () => {
+        const response = await request(app).get('/');
+        expect(response.statusCode).toBe(200);
+        expect(response.text).toBe('Hello, Jenkins Pipeline!');
+    });
+});
