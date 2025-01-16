510a50da826bdee613b4d0e8be69cee4aa54c2e4
Tejalkale13 <166007676+Tejalkale13@users.noreply.github.com>
2025-01-16 12:19:56 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const express = require('express');
+/*const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
@@ -10,4 +10,22 @@ app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
 
-module.exports = app; // Exporting app for unit testing
+module.exports = app; // Exporting app for unit testing*/
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello, Jenkins Pipeline!');
+});
+
+// Export the app for testing
+if (process.env.NODE_ENV !== 'test') {
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+}
+
+module.exports = app; // Exporting app for testing
+
