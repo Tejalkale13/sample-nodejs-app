1119aa632e44f3bc3de6d55430bf30f2623dd56f
Tejalkale13 <166007676+Tejalkale13@users.noreply.github.com>
2025-01-16 12:20:43 +0530

    Update app.test.js

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,3 +1,13 @@
+/*const request = require('supertest');
+const app = require('../app'); // Import the Express app for testing
+
+describe('GET /', () => {
+    it('should return "Hello, Jenkins Pipeline!"', async () => {
+        const response = await request(app).get('/');
+        expect(response.statusCode).toBe(200);
+        expect(response.text).toBe('Hello, Jenkins Pipeline!');
+    });
+});*/
 const request = require('supertest');
 const app = require('../app'); // Import the Express app for testing
 
@@ -8,3 +18,4 @@ describe('GET /', () => {
         expect(response.text).toBe('Hello, Jenkins Pipeline!');
     });
 });
+
